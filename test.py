a48211d1dc70303d03a9a4b6c9d5e344b1ff2432
Dagrol <pav426@gmail.com>
2016-02-17 17:01:43 +0000

    meh

---
+++ test.py
@@ -0,0 +1,26 @@
+import urllib2
+import json
+import time
+ 
+def fetchPreMarket(symbol, exchange):
+    link = "http://finance.google.com/finance/info?client=ig&q="
+    url = link+"%s:%s" % (exchange, symbol)
+    u = urllib2.urlopen(url)
+    content = u.read()
+    data = json.loads(content[3:])
+    info = data[0]
+    t = str(info["lt"])
+    l = float(info["l"])
+    p = float(info["l_fix"])
+    return (t,l,p)
+ 
+ 
+p0 = 0
+while True:
+    t, l, p = fetchPreMarket("GOOG","NASDAQ")
+    if(p!=p0):
+        p0 = p
+        print("%s\t%.2f\t%.2f\t%+.2f\t%+.2f%%" % (t, l, p, p-l,
+                                                 (p/l-1)*100.))
+    time.sleep(1)
+
